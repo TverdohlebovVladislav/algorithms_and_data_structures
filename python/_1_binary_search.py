3b904bab5f8e68518c25f163db8814585978538d
TverdohlebovVladislav <tverdohlebov-work@yandex.ru>
2022-01-17 20:47:52 +0300

    Add selection sort

--- python/_1_binary_search.py
+++ python/_1_binary_search.py
@@ -1,13 +1,39 @@
 # Test array
 list1 = [1, 2, 5, 7, 8, 12, 14, 16, 24]
+list2 = [34, 1, 23, 47, 32, 2, 43, 7, 9]
+
+# ---
+# Selection sort
+def slection_sort(arr):
+    # 1. Find the smallest
+    # 2. Insert the smallest el in new list
+    # 3. Repeat until all elements are finished 
+    def find_min(arr):
+        minimal_el = arr[0]
+        min_index = 0
+        for i in range(len(arr)):
+            if arr[i] < minimal_el:
+                minimal_el = arr[i]
+                min_index = i
+        return min_index
+    output = []
+    for i in range(len(arr)):
+        smallest = find_min(arr)
+        output.append(arr.pop(smallest))
+    return output
+
 
 # ---
 # Binary search
 def binary_search(arr, el_to_find):
-    # 1. Find the middle
-    # 2. ELement is higher or lower?
-    # 3. Take the corresponding part and find the middle
-    # 4. Go to the first step until you find the right one or the remainder will become less than zero.
+    # 1. Sort an array
+    # 2. Find the middle
+    # 3. ELement is higher or lower?
+    # 4. Take the corresponding part and find the middle
+    # 5. Go to the first step until you find the right one or the remainder will become less than zero.
+    arr = slection_sort(arr)
+    print("Отсортированный массив:", arr)
+
     first = 0;
     last = len(arr) - 1
 
@@ -23,4 +49,4 @@ def binary_search(arr, el_to_find):
     return "Элемент отсутствует!"
     
 
-print("Найденный элемент (индекс в массиве): " + str(binary_search(list1, 5)))
+print("Найденный элемент (индекс в массиве): " + str(binary_search(list2, 47)))
